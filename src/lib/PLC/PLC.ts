876aecb6c4edd22069027684f3751947a4c628de
dkleber89 <dkleber89@gmail.com>
2022-09-25 16:11:51 +0200

    First implementation of PLC Class

--- src/lib/PLC/PLC.ts
+++ src/lib/PLC/PLC.ts
@@ -1,19 +1,130 @@
-import { AdsClientConnectOptions } from 'node-ads';
+import { AdapterInstance } from '@iobroker/adapter-core';
+import { AdsClient, AdsClientConnectOptions, AdsReadDeviceInfoResult, connect } from 'node-ads';
 
 export class PLC {
-    private adsClientConnectOptions: AdsClientConnectOptions;
-    // private adsClient: AdsClient;
+    private _adapter: Pick<AdapterInstance, 'log' | 'setState'>;
+    private _adsClientConnectOptions: AdsClientConnectOptions;
+    private _reconnectInterval: number;
 
-    constructor(adsClientConnectOptions: AdsClientConnectOptions) {
-        this.adsClientConnectOptions = adsClientConnectOptions;
+    private _adsClient: AdsClient;
+    private _checkDeviceStateInterval: NodeJS.Timer | null = null;
+    private _reconnectTimer: NodeJS.Timeout | null = null;
 
-        // this.adsClient = connect(this.adsClientConnectOptions, () => {
-        //     this.adsClient.readDeviceInfo((err, result) => {
-        //         console.log(err);
-        //         console.log(result);
-        //     });
+    public connected = false;
+    public deviceInfo: AdsReadDeviceInfoResult | null = null;
 
-        //     this.adsClient.end();
-        // });
+    constructor(adapter: AdapterInstance, adsClientConnectOptions: AdsClientConnectOptions, reconnectInterval: number) {
+        this._adapter = { log: adapter.log, setState: adapter.setState };
+        this._adsClientConnectOptions = adsClientConnectOptions;
+        this._reconnectInterval = reconnectInterval;
+
+        this._adapter.setState('info.connection', this.connected, true);
+
+        this._adapter.log.info(`Connecting to "${this._adsClientConnectOptions.host}"`);
+        this._adsClient = connect(
+            {
+                verbose: this._adapter.log.level === 'debug' ? 1 : this._adapter.log.level === 'silly' ? 2 : 0,
+                ...this._adsClientConnectOptions,
+            },
+            this._onConnected,
+        );
+
+        this._adsClient.on('timeout', (error) => {
+            this._adapter.log.error(`Timeout occured in AdsClient: ${error}`);
+
+            this._onDisconnecting();
+        });
+
+        this._adsClient.on('error', (error) => {
+            this._adapter.log.error(`Error ocurred in AdsClient: ${error}`);
+
+            this._onDisconnecting();
+        });
+    }
+
+    private _onConnected(): void {
+        this.connected = true;
+        this._adapter.setState('info.connection', this.connected, true);
+        this._adapter.log.info(`Connection to "${this._adsClientConnectOptions.host}" established`);
+
+        this._checkDeviceStateInterval = setInterval(() => {
+            this._adsClient.readDeviceInfo((error, result) => {
+                if (error) {
+                    this._adapter.log.error(`Error on check Device state occurred: ${error.message}`);
+
+                    this._onDisconnecting();
+                }
+
+                if (result) {
+                    this.deviceInfo = result;
+                }
+            });
+        }, 5000);
+    }
+
+    private _onDisconnecting(): void {
+        this._adapter.log.info(`Disconnect from "${this._adsClientConnectOptions.host}"`);
+        if (this._checkDeviceStateInterval) {
+            clearInterval(this._checkDeviceStateInterval);
+
+            this._checkDeviceStateInterval = null;
+        }
+
+        this.connected = false;
+        this._adapter.setState('info.connection', this.connected, true);
+        this.deviceInfo = null;
+
+        this._adsClient.end(() => {
+            this._adapter.log.debug(
+                `Disconnect from "${this._adsClientConnectOptions.host}" done, start reconnect timer`,
+            );
+            this._reconnect();
+        });
+    }
+
+    private _reconnect(): void {
+        if (!this._reconnectTimer && !this.connected) {
+            this._reconnectTimer = setTimeout(() => {
+                if (!this.connected) {
+                    this._adapter.log.info(`Connecting to "${this._adsClientConnectOptions.host}"`);
+                    this._adsClient.connect(() => {
+                        if (this._reconnectTimer) {
+                            clearTimeout(this._reconnectTimer);
+                            this._reconnectTimer = null;
+                        }
+
+                        this._onConnected();
+                    });
+                }
+            }, this._reconnectInterval * 1000);
+        }
+    }
+
+    public closeConnection(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const timeout = setTimeout(() => {
+                this._adapter.log.error('Timeout closing AdsClient connection');
+                reject();
+            }, 2000);
+
+            this._adapter.log.info('Close AdsClient connection');
+            if (this._checkDeviceStateInterval) {
+                clearInterval(this._checkDeviceStateInterval);
+                this._checkDeviceStateInterval = null;
+            }
+
+            if (this._reconnectTimer) {
+                clearTimeout(this._reconnectTimer);
+                this._reconnectTimer = null;
+            }
+
+            if (this._adsClient) {
+                this._adsClient.end(() => {
+                    clearTimeout(timeout);
+                    this._adapter.log.info('Close AdsClient connection done');
+                    resolve();
+                });
+            }
+        });
     }
 }
